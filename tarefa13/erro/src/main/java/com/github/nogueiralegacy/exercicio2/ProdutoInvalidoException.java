5de987a3e1cc5b2525d692f8046a82c422314aa5
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-06-02 23:47:52 -0300

    Lidando com exceção personalizada

---
+++ tarefa13/erro/src/main/java/com/github/nogueiralegacy/exercicio2/ProdutoInvalidoException.java
@@ -0,0 +1,8 @@
+package com.github.nogueiralegacy.exercicio2;
+
+public class ProdutoInvalidoException extends Exception {
+
+    public ProdutoInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
